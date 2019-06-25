0ebc66bb6dd2ae46f97cec38461d4e73807fafc6
Karla Lenfeld <karlalenfeld@gmail.com>
2019-06-25 14:26:47 +0800

    new commit
    All changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,11 @@ $(document).ready(function(){
     $(this).toggleClass('active');
     $('.menu').toggleClass('hidden')
     });
+
+    var typed = new Typed('#typed', {
+	stringsElement: '#typed-strings',
+	loop: true,
+   loopCount: Infinity,
+	typeSpeed: 100
+});
 });
